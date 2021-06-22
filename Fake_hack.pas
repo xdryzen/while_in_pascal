915d018c5287ca94923a94254d501f07851c3d16
DEVL_NOOB <69730462+yunanaexploit@users.noreply.github.com>
2021-06-22 08:31:20 +0000

    Create Fake_hack.pas

---
+++ Fake_hack.pas
@@ -0,0 +1,41 @@
+program fake_hack_pascal;
+
+uses
+crt;
+
+var
+a:String;
+i:integer=10;
+w:string='hacking';
+{
+AUTHOR : AHMAD
+CODING : UTF-8
+GITHUB : YUNANAEXPLOIT
+FACEBOOK : wRyZ.freands.158.watded.daww
+}
+begin
+    writeln('######################');
+    writeln('## PASCAL   HACKING ##');
+    writeln('######################');
+    writeln('');
+    writeln('');
+    textcolor(red);
+    writeln('    1.hack');
+    writeln('    0.exit');
+    writeln('');
+    write('pilih: ');
+    read(a);
+    if(a = '1')then
+        while i < 20 do
+        
+        begin
+            textcolor(green);
+            writeln('passwordfound,succes');
+            i:=i+1;
+            delay(100)
+        end
+    else
+        exit;
+        
+readln;
+end.
